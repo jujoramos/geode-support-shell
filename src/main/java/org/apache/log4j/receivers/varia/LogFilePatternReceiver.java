135bcdb52e3d4354b7c5457b04a8f1ff3c8ca3e4
Juan José Ramos Cassella <jujoramos@gmail.com>
2018-11-02 10:11:22 +0000

    - Added missing License Header to some files.

--- src/main/java/org/apache/log4j/receivers/varia/LogFilePatternReceiver.java
+++ src/main/java/org/apache/log4j/receivers/varia/LogFilePatternReceiver.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.log4j.receivers.varia;
 
 import java.io.BufferedReader;
